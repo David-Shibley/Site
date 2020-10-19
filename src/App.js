33ceaf69a7380026813b0574600e98ec1b938fc2
David Shibley <davidjshibley@gmail.com>
2020-10-19 00:26:26 -0600

    doing some refactoring and starting work on landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
-import Header from './Header.js';
-import './App.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import DailySites from './Daily_Runner/DailySites';
+import Portfolio from './Portfolio';
 
-const App = props => {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div className="App">
-      <Header className="App-header" />
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (<div>{children}</div>)}
     </div>
-  )
+  );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  navBar: {
+    color: "#071013",
+    backgroundColor: "#23b5d3",
+    position: "static",
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.navBar}>
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="Porfolio" {...a11yProps(0)} />
+          <Tab label="Daily Site Runner" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Portfolio />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <DailySites />
+      </TabPanel>
+    </div>
+  );
+}
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import logo from './getting.work.done.gif';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Avatar, Card, CardHeader, CardContent, CardActions, Button, Typography, Link } from '@material-ui/core';
+
+import DoWorkLogo from './getting.work.done.gif';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+    width: '320px',
+    height: '480px',
+    '&:hover .flipper, .flip-container.hover .flipper': {
+        transform: 'rotateY(180deg)',
+      }
+  },
+  avatar: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+    animation: 'App-logo-spin infinite 20s linear',
+  },
+}));
+
 const Portfolio = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <h1>Portfolio</h1>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
+    <Container className={classes.root}>
+      <Card class="flipper" ontouchstart="this.classList.toggle('hover');">
+        <CardHeader>Working</CardHeader>
+        <CardContent class="front">
+          <Avatar src={DoWorkLogo} className={classes.avatar} alt="logo" tooltip="working" />
+          <Typography>Page is currently being worked on</Typography>
+          <CardActions>
+            <Button>Hover here to learn more</Button>
+          </CardActions>
+        </CardContent>
+        <CardContent class="back">
+          <Avatar src={DoWorkLogo} className={classes.avatar} alt="logo" tooltip="working" />
+          <Typography>See the code on github</Typography>
+          <CardActions>
+            <Link src="https://github.com/David-Shibley?tab=repositories">Hover here to learn more</Link>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </Container>
   )
 }
 
